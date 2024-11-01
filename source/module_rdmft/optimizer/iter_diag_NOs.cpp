e0b61d808de8dabf44cab35648d7f02da56eac94
jghan <jghan@iphy.ac.cn>
2024-11-01 22:40:49 +0800

    add iterDiag_rdmft

---
+++ source/module_rdmft/optimizer/iter_diag_NOs.cpp
@@ -0,0 +1,31 @@
+//==========================================================
+// Author: Jingang Han
+// DATE : 2024-11-01
+//==========================================================
+
+// #include "module_rdmft/rdmft.h"
+#include "module_rdmft/optimizer/iter_diag_NOs.h"
+
+namespace rdmft
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
---
+++ source/module_rdmft/optimizer/iter_diag_NOs.h
@@ -0,0 +1,33 @@
+//==========================================================
+// Author: Jingang Han
+// DATE : 2024-11-01
+//==========================================================
+#ifndef ITER_DIAG_NOS_H
+#define ITER_DIAG_NOS_H
+
+#include "module_rdmft/rdmft.h"
+
+namespace rdmft
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+#endif
+
