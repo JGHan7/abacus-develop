1da62c3e69f466decec03e076c542845e75f83e9
jghan <jghan@iphy.ac.cn>
2025-01-05 00:22:44 +0800

    save

--- source/module_rdmft/esolver_rdmft.cpp
+++ source/module_rdmft/esolver_rdmft.cpp
@@ -215,7 +215,7 @@ void ESolver_RDMFT<TK, TR>::runner(UnitCell& ucell, const int istep)
 
     }
 
-    std::cout << std::scientific << std::setprecision(3) << std::endl;
+    std::cout << std::scientific << std::setprecision(1) << std::endl;
     rdmft::printMatrix_pointer(rdmft_solver.nk_total, rdmft_solver.nbands_total, rdmft_solver.occ_number.c, "occ_number", 10);
     std::cout << std::defaultfloat;
 
