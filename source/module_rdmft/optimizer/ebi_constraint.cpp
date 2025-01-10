34821c89dc5a64bc012ce7ea8857defe28d1814d
jghan <jghan@iphy.ac.cn>
2025-01-10 17:22:13 +0800

    last save before leaving HZW&para

--- source/module_rdmft/optimizer/ebi_constraint.cpp
+++ source/module_rdmft/optimizer/ebi_constraint.cpp
@@ -331,7 +331,7 @@ void EBI::solving_mu()
         while( std::abs(occ_num_error) > this->tot_nelec_thr )
         {
             ++this->solve_mu_times;
-            if( solve_mu_times > 100 )
+            if( solve_mu_times > 200 )
             {
                 std::cout << "\nThe number of times to solve mu using the Newton's method is too many: " << solve_mu_times << "\n" << std::endl;
                 break;
