b5c6d269ff5c085f4fbd191171ca7ec5723d5163
jghan <jghan@iphy.ac.cn>
2025-09-25 13:20:57 +0800

    Fix: class BFGS_Opti

--- source/module_rdmft/optimizer/bfgs_opti.cpp
+++ source/module_rdmft/optimizer/bfgs_opti.cpp
@@ -60,7 +60,7 @@ void BFGS_Opti<TX>::get_pk(const std::vector<TX>& dE_dx_new, const std::vector<T
     // set some vars zero?
 
     // get Hk
-    this->cal_Hk(dE_dx_new, x_new);
+    this->cal_Hk(dE_dx_new, x_new, new_landscape);
     // if( new_landscape || a_equal_b(x_new, this->var_x) )
     // {
     //     std::fill(this->var_x.begin(), this->var_x.end(), 0.0);
@@ -79,52 +79,51 @@ void BFGS_Opti<TX>::get_pk(const std::vector<TX>& dE_dx_new, const std::vector<T
     // }
     // else
     // {
-    //     this->cal_Hk(dE_dx_new, x_new);
-    //     // for(int j=0; j<x_new.size(); ++j)
-    //     // {
-    //     //     // diff_x, sk = x_k+1 - x_k
-    //     //     this->diff_x[j] = x_new[j] - this->var_x[j];
-    //     //     // diff_grad, yk = (dE_dx)_k+1 - (dE_dx)_k
-    //     //     this->diff_grad[j] = dE_dx_new[j] - this->dE_dx[j];
-    //     // }
+    //     for(int j=0; j<x_new.size(); ++j)
+    //     {
+    //         // diff_x, sk = x_k+1 - x_k
+    //         this->diff_x[j] = x_new[j] - this->var_x[j];
+    //         // diff_grad, yk = (dE_dx)_k+1 - (dE_dx)_k
+    //         this->diff_grad[j] = dE_dx_new[j] - this->dE_dx[j];
+    //     }
 
-    //     // if( PARAM.inp.print_BFGS_Hk )
-    //     // {
-    //     //     rdmft::printMatrix_pointer(nk_total, nbands, this->diff_x.data(), "in BFGS_Opti, diff_x");
-    //     //     rdmft::printMatrix_pointer(nk_total, nbands, this->diff_grad.data(), "in BFGS_Opti, diff_grad");
-    //     // }
+    //     if( PARAM.inp.print_BFGS_Hk )
+    //     {
+    //         rdmft::printMatrix_pointer(nk_total, nbands, this->diff_x.data(), "in BFGS_Opti, diff_x");
+    //         rdmft::printMatrix_pointer(nk_total, nbands, this->diff_grad.data(), "in BFGS_Opti, diff_grad");
+    //     }
 
-    //     // char op_tran = 'T';
-    //     // if constexpr (std::is_same<TX, std::complex<double>>::value)
-    //     // {
-    //     //     op_tran = 'C';
-    //     // }
+    //     char op_tran = 'T';
+    //     if constexpr (std::is_same<TX, std::complex<double>>::value)
+    //     {
+    //         op_tran = 'C';
+    //     }
 
-    //     // // cal rho = 1/( diffGrad^T * diffX )
-    //     // TX rho_temp = 0.0;
-    //     // rdmft::Tgemm_lapack(this->diff_grad.data(), this->diff_x.data(), &rho_temp, 1, 1, this->dim, op_tran, 'N');
-    //     // this->rho = 1.0/std::real(rho_temp);
+    //     // cal rho = 1/( diffGrad^T * diffX )
+    //     TX rho_temp = 0.0;
+    //     rdmft::Tgemm_lapack(this->diff_grad.data(), this->diff_x.data(), &rho_temp, 1, 1, this->dim, op_tran, 'N');
+    //     this->rho = 1.0/std::real(rho_temp);
 
-    //     // if( std::real(rho_temp) < 1e-10 ) { } //////////////////////////
+    //     if( std::real(rho_temp) < 1e-10 ) { } //////////////////////////
 
-    //     // std::cout << "******\n" << "in BFGS_Opti::get_pk(), 1.0/rho = y^/dagger s: " << rho_temp << "\n******" << std::endl;
-    //     // std::cout << "******\n" << "in BFGS_Opti::get_pk(), rho: " << this->rho << "\n******" << std::endl;
+    //     std::cout << "******\n" << "in BFGS_Opti::get_pk(), 1/rho = y^/dagger s: " << rho_temp << "\n******" << std::endl;
+    //     std::cout << "******\n" << "in BFGS_Opti::get_pk(), rho: " << this->rho << "\n******" << std::endl;
 
-    //     // rho_temp = this->rho;
-    //     // // cal rho * diffX * diffGrad^T, I - rho * diffX * diffGrad^T
-    //     // rdmft::Tgemm_lapack( this->diff_x.data(), this->diff_grad.data(), this->rho_diffX_diffGrad.data(), this->dim, this->dim, 1, 'N', op_tran, -(rho_temp) );
-    //     // for(int i=0; i<this->dim; ++i) { this->rho_diffX_diffGrad[i*this->dim + i] += 1.0; }
+    //     rho_temp = this->rho;
+    //     // cal rho * diffX * diffGrad^T, I - rho * diffX * diffGrad^T
+    //     rdmft::Tgemm_lapack( this->diff_x.data(), this->diff_grad.data(), this->rho_diffX_diffGrad.data(), this->dim, this->dim, 1, 'N', op_tran, -(rho_temp) );
+    //     for(int i=0; i<this->dim; ++i) { this->rho_diffX_diffGrad[i*this->dim + i] += 1.0; }
 
-    //     // // cal rho * diffX * diffX^T
-    //     // rdmft::Tgemm_lapack( this->diff_x.data(), this->diff_x.data(), this->rho_diffX_diffX_T.data(), this->dim, this->dim, 1, 'N', op_tran, rho_temp );
+    //     // cal rho * diffX * diffX^T
+    //     rdmft::Tgemm_lapack( this->diff_x.data(), this->diff_x.data(), this->rho_diffX_diffX_T.data(), this->dim, this->dim, 1, 'N', op_tran, rho_temp );
 
-    //     // // cal H_k+1 = (I - rho * diffX * diffGrad^T) * H_k * (I - rho * diffX * diffGrad^T)^T + rho * diffX * diffX^T
-    //     // std::vector<TX> H_tmp = this->Hk;
-    //     // rdmft::Tgemm_lapack( this->rho_diffX_diffGrad.data(), this->Hk.data(), H_tmp.data(), this->dim, this->dim, this->dim );
-    //     // rdmft::Tgemm_lapack( H_tmp.data(), this->rho_diffX_diffGrad.data(), this->Hk.data(), this->dim, this->dim, this->dim, 'N', op_tran );
-    //     // // rdmft::Tgemm_lapack( this->rho_diffX_diffGrad.data(), this->Hk.data(), this->Hk.data(), this->dim, this->dim, this->dim );
-    //     // // rdmft::Tgemm_lapack( this->Hk.data(), this->rho_diffX_diffGrad.data(), this->Hk.data(), this->dim, this->dim, this->dim, 'N', op_tran );
-    //     // for(int i=0; i<this->Hk.size(); ++i) { this->Hk[i] += this->rho_diffX_diffX_T[i]; }
+    //     // cal H_k+1 = (I - rho * diffX * diffGrad^T) * H_k * (I - rho * diffX * diffGrad^T)^T + rho * diffX * diffX^T
+    //     std::vector<TX> H_tmp = this->Hk;
+    //     rdmft::Tgemm_lapack( this->rho_diffX_diffGrad.data(), this->Hk.data(), H_tmp.data(), this->dim, this->dim, this->dim );
+    //     rdmft::Tgemm_lapack( H_tmp.data(), this->rho_diffX_diffGrad.data(), this->Hk.data(), this->dim, this->dim, this->dim, 'N', op_tran );
+    //     // rdmft::Tgemm_lapack( this->rho_diffX_diffGrad.data(), this->Hk.data(), this->Hk.data(), this->dim, this->dim, this->dim );
+    //     // rdmft::Tgemm_lapack( this->Hk.data(), this->rho_diffX_diffGrad.data(), this->Hk.data(), this->dim, this->dim, this->dim, 'N', op_tran );
+    //     for(int i=0; i<this->Hk.size(); ++i) { this->Hk[i] += this->rho_diffX_diffX_T[i]; }
 
     // }
 
@@ -215,7 +214,7 @@ void BFGS_Opti<TX>::cal_Hk(const std::vector<TX>& dE_dx_new, const std::vector<T
         }
 
         const char op_tran = std::is_same<TX, std::complex<double>>::value ? 'C' : 'T';
-        
+
         // cal rho = 1/( diffGrad^T * diffX )
         TX rho_temp = 0.0;
         rdmft::Tgemm_lapack(this->diff_grad.data(), this->diff_x.data(), &rho_temp, 1, 1, this->dim, op_tran, 'N');
